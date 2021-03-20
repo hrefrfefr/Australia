bb270f4cf9d40098a4453ab272962244c5854321
hrefrfefr <tf1max@gmail.com>
2021-03-20 18:32:29 +0400

    1

--- Australia.py
+++ Australia.py
@@ -3,8 +3,7 @@ from Samples.mapapi_PG import show_map
 
 def show_maps():
     map_locations = {
-        # "Австралия": ("ll=135.746181,-27.483765&spn=20,20", "sat"),
-        "Алматы": ("ll=76.964531,43.242508&z=10", "map")
+        "Австралия": ("ll=135.746181,-27.483765&spn=20,20", "sat")
     }
     for map_location, map_type in map_locations.values():
         show_map(map_location, map_type)
